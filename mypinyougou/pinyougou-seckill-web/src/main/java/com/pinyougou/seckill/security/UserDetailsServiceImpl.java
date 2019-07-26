c1d5e884220ca9e4d4ed0c72572222454e1ce229
XiaoD-00 <630602884@qq.com>
2019-07-26 16:43:02 +0800

    房靖滔-用户三个月不登录，冻结，功能完成（完善到其他模块）

--- mypinyougou/pinyougou-seckill-web/src/main/java/com/pinyougou/seckill/security/UserDetailsServiceImpl.java
+++ mypinyougou/pinyougou-seckill-web/src/main/java/com/pinyougou/seckill/security/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pinyougou.seckill.security;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.user.service.LoginService;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,8 +9,19 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Reference
+    private LoginService loginService;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new User(s,"", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Boolean flag = loginService.login(username);
+        if (flag) {
+            //只做授权，认证交给CAS
+            return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+        }else {
+            //用户已冻结
+            return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("NONE"));
+        }
     }
 }
--- mypinyougou/pinyougou-user-web/src/main/java/com/pinyougou/user/security/UserDetailsServiceImpl.java
+++ mypinyougou/pinyougou-user-web/src/main/java/com/pinyougou/user/security/UserDetailsServiceImpl.java
@@ -1,5 +1,8 @@
 package com.pinyougou.user.security;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.user.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,8 +13,19 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
  * 该类中的方法只帮我们做授权，认证交给CAS服务器
  */
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Reference
+    private LoginService loginService;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new User(s,"", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Boolean flag = loginService.login(username);
+        if (flag) {
+            //只做授权，认证交给CAS
+            return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+        }else {
+            //用户已冻结
+            return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("NONE"));
+        }
     }
 }
